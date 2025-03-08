b1ab98dd25db1d2ecd7ff96ebed1180fc030ab59
Jiya Duseja <jiyaduseja@Jiyas-MacBook-Air.local>
2025-03-08 11:57:47 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const admin =require("firebase-admin");
 const app=express();
 const PORT=3000;
 app.use(express.json());
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_KEY, "base64").toString("utf-8"));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
